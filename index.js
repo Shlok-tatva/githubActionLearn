22737634d5087aaf5bd30d241e085bb3952f7722
Shlok-tatva <shlok.jadeja@etatvasoft.com>
2024-07-29 13:31:41 +0530

    dummy change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function sum(a, b) {
     return a + b;
 }
 
-console.log("This change done in Feature Branch")
+console.log("This change done in Feature Branch v2")
 
 // Export the function to make it accessible in other files
 module.exports = sum;
